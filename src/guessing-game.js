025089d83fada97365acb39c3b743868203938fc
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-04 01:58:32 +0600

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.num = Math.round((this.min + this.max) / 2);
+    return this.num;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.num;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.num;
+  }
 }
 
 module.exports = GuessingGame;
